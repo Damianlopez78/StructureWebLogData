17d3bd6d9d1476df280e23b36ae195781251b4b8
rtaha07 <37188210+rtaha07@users.noreply.github.com>
2018-04-22 22:15:18 -0400

    Add files via upload

---
+++ CS631/latest code/ContactUs_Process.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>Contact Us</title>
+<link rel="STYLESHEET" type="text/css" href="contact-us.css" />
+</head>
+<body>
+
+<h1>Thank you for your Interest. Your Information are below.</h1> 
+
+<?php
+
+echo "<br /><strong>Your First name:</strong> " . $_POST["firstname"]; 
+
+echo "<br /><strong>Your Last name:</strong> " . $_POST["lastname"]; 
+
+echo "<br /><strong>Email Address:</strong> " . $_POST["email"]; 
+     
+echo "<br /><strong>Your Message:</strong> " . $_POST["message"]; 
+
+?> 
+
+
+</body>
+</html>
